b525e9054bbfa7bfd44e90a6e2da541cfbf410b9
Daniel Candido <danielcandidocel@gmail.com>
2018-02-15 06:13:59 -0200

    Configurando Layout - Header

--- views/404.php
+++ views/404.php
@@ -1,1 +1,1 @@
-<h1>Pagina não encontrada</h1>
+<h1>Inoveh - Pagina não encontrada</h1>
--- views/template.php
+++ views/template.php
@@ -15,71 +15,61 @@
     </head>
 <body>
     <header>
-        <div class="container">
+        <div class="topo">
             <div class="logo">
                 <a href="">
-                    <img src="<?php echo BASE_URL; ?>assets/images/logo_inoveh.png" />
+                    <img src="<?php echo BASE_URL;?>assets/images/logo_inoveh.png" />
                 </a>
             </div>
-            <div class="topo">
-                <div class="nav1">
-                    <nav>
-    <!--                    <div class="menuMobile">
-                            <div class="mm_line"></div>
-                            <div class="mm_line"></div>
-                            <div class="mm_line"></div>
-                        </div>-->
-                        <ul>
-                            <li><a href="">Login ou Cadastre-se</a></li>
-                            <li><a href="">Meus Pedidos</a></li>
-                            <li><a href="">Central de Atendimento</a></li>
-                            <li><a href="" id="televendas">Televendas (35) 99999-9999</a></li>                            
-                        </ul>
-                    </nav>
+            <div class="topo-menu">
+                <div class="topo-info">
+                    <ul>
+                        <li id="normal"><a href="">Faça Login ou Cadastre-se</a></li>
+                        <li><a href="">Meus Pedidos</a></li>
+                        <li><a href="">Central de Atendimento</a></li>
+                        <li id="tel">Televendas (35) 99999-9999</li>
+                    </ul>
                 </div>
-                <div class="nav2">
-                    <div class="buscar">
-                        <input type="text" name="search" />
-                        <button>Buscar</button>
+                <div class="busca">
+                    <div class="campo-busca">
+                        <form>
+                            <input type="text" id="busca" placeholder="Digite o que procura"/>
+                            <input type="button" value="Buscar" id="button" class="button"/>
+                        </form>
                     </div>
                     <div class="cart">
-                        <span class="glyphicon glyphicon-shopping-cart"></span>
-                        <div class="cartqt">
-                            <?PHP echo (isset($viewData['qt_carrinho']))?$viewData['qt_carrinho']:'0';?>
-                        </div>
+                        <a href="<?php echo BASE_URL; ?>cart">
+                            <div class="cartarea">
+                                <span class="glyphicon glyphicon-shopping-cart"></span>
+                                <div class="cartqt">
+                                    <?PHP echo (isset($viewData['qt_carrinho']))?$viewData['qt_carrinho']:'0';?>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
-        </div>        
-    </header>
-    <div class="menu">
-<!--            <nav>
-                    <div class="menuMobile">
-                        <div class="mm_line"></div>
-                        <div class="mm_line"></div>
-                        <div class="mm_line"></div>
-                    </div>
+        </div> 
+        <div class="menu">
+            <nav>
                 <ul>
-                    <li class="active"><a href="">Balança</a></li>
-                    <li><a href="">Automação</a></li>
+                    <li><a href="">Balanças</li>
+                    <li><a href="">Suprimentos</a></li>
                     <li><a href="">Informática</a></li>
-                    <li><a href="">Suprimento</a></li>                            
+                    <li style="border-right: 2px solid #FFF"><a href="">Automação</a></li>
                 </ul>
-            </nav>-->
+            </nav>
         </div>
-    <section id="banner">
-        
-    </section>
-    <section id="corpo">
-        <div class="container">
-            
-        </div>
-    </section>
+    </header>
+
               
      
 <footer>
     
 </footer>
+    <script type="text/javascript">
+        var BASE_URL = '<?php echo BASE_URL; ?>';
+    </script>
     <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery.zoom.js"></script>
     <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery.zoom.min.js"></script>
     <script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-ui.min.js"></script>
